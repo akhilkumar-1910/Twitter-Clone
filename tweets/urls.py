e597cb6d2f9a3d0ed8f544842466130ff5e9ca5e
Akhil <akhilkumar1910@gmail.com>
2020-03-10 14:09:53 +0530

    Modified home, my_tweets, added user_tweets

--- tweets/urls.py
+++ tweets/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('home', views.home, name='home'),
     path('my_tweets', views.my_tweets, name='my_tweets'),
+    path('user_tweets/<str:user>', views.user_tweets, name='user_tweets'),
     path('create_tweet', views.create_tweet, name='create_tweet'),
+    path('remove_tweet', views.remove_tweet, name='remove_tweet'),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -4,24 +4,29 @@ from .models import Tweet, Tag
 
 # Create your views here.
 def home(request):
-    all_tweets = Tweet.objects.all()
-    print(request.POST)
-    return render(request, 'home.html', context={'all_tweets': all_tweets})
+    print(request.user)
+    all_tweets = Tweet.objects.all().order_by('-last_edited')
+    users = User.objects.all()
+    return render(request, 'home.html', context={'all_tweets': all_tweets, 'users': users})
 
 def my_tweets(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        user_tweets = Tweet.objects.filter(user__username=username)
-        return render(request, 'my_tweets.html', context={'user_tweets': user_tweets})
-    else:
-        return redirect('home')
+    username = request.user.username
+    my_tweets = Tweet.objects.filter(user__username=username).order_by('-last_edited')
+    users = User.objects.all()
+    return render(request, 'my_tweets.html', context={'my_tweets': my_tweets, 'users': users})
+
+def user_tweets(request, user):
+    user_tweets = Tweet.objects.filter(user__username=user).order_by('-last_edited')
+    users = User.objects.all()
+    return render(request, 'user_tweets.html', context={'user_tweets': user_tweets, 'users': users})
+
 
 def create_tweet(request):
     if request.method == "POST":
         message = request.POST.get("message")
         tags = request.POST.get("tag")
         tags = tags.split()
-        username=request.POST.get("username")
+        username=request.user.username
         user = User.objects.filter(username = username)
         #print(user[0])
         tweet = Tweet(message=message, user=user[0])
@@ -29,6 +34,14 @@ def create_tweet(request):
         for tag in tags:
             t = Tag(tag_name=tag, tweet=tweet)
             t.save()
-        return redirect('home')
+        return redirect('my_tweets')
     else:
-        return redirect('home')
+        return redirect('my_tweets')
+
+def remove_tweet(request):
+    if request.method == "POST":
+        tweet_id = request.POST.get("tweet_id")
+        Tweet.objects.filter(id=tweet_id).delete()
+        return redirect('my_tweets')
+    else:
+        return redirect('my_tweets')
